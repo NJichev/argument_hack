61a18827784c82500a5fb4bf427aa0a5635d5082
Nikola Jichev <njichev@gmail.com>
2018-04-13 18:40:50 +0300

    Use unquote splicing.

--- lib/sum_all.ex
+++ lib/sum_all.ex
@@ -1,17 +1,7 @@
 defmodule SumAll do
   defmacro sum({:all, _env, values}) do
-    res =
-      values
-      |> Enum.map(&Macro.expand(&1, __CALLER__))
-      |> Enum.reduce(fn value, acc ->
-        case value do
-          {:-, _, [v]} -> acc - v
-          _ -> value + acc
-        end
-      end)
-
     quote do
-      unquote(res)
+      Enum.sum([unquote_splicing(values)])
     end
   end
 end
